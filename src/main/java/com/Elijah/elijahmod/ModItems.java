1ac3820b54cb1ba7292b65172ecf4e5fe752c374
Elijah Busch <elijah1busch@gmail.com>
2026-03-19 14:05:58 -0500

    Add HammerItem to ModItems
    Register a new HammerItem with a durability of 500.

--- src/main/java/com/Elijah/elijahmod/ModItems.java
+++ src/main/java/com/Elijah/elijahmod/ModItems.java
@@ -1,1 +1,20 @@
+package com.elijah.elijahmod;
+
+import com.elijah.elijahmod.item.HammerItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, "elijahmod");
+
+    public static final RegistryObject<Item> HAMMER = ITEMS.register("hammer",
+            () -> new HammerItem(new Item.Properties().durability(500)));
+
+}
 
